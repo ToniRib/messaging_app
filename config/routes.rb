5bf0bf225686d3e3e8cec7e95181c19194c27292
ToniRib <tonimarierib@gmail.com>
2016-03-23 17:59:59 -0600

    Set up controllers so user can create an account

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+  resources :users, only: [:new, :create]
+  get '/dashboard', to: 'dashboard#show'
 end
--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -3,17 +3,20 @@ require "rails_helper"
 RSpec.describe "Create account", type: :feature do
   scenario "new user creates account" do
     visit root_path
-    click on "Create Account"
+    click_on "Create an Account"
+
+    expect(current_path).to eq(new_user_path)
 
     fill_in "Username", with: "tonirib"
     fill_in "First name", with: "Toni"
     fill_in "Last name", with: "Rib"
-    fill_in "Email address", with: "toni@example.com"
+    fill_in "Email", with: "toni@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
 
     click_on "Create My Account"
 
-    
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Welcome, Toni")
   end
 end
