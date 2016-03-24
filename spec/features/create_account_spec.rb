0c8acab512ac3cbb554c6b2a13a28df1ca2429db
ToniRib <tonimarierib@gmail.com>
2016-03-23 18:21:13 -0600

    Additional coloring/styling for the new account page

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -13,10 +13,25 @@ RSpec.describe "Create account", type: :feature do
     fill_in "Email", with: "toni@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-
     click_on "Create My Account"
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Welcome, Toni")
+  end
+
+  scenario "password and password confirmation do not match" do
+    visit new_user_path
+
+    fill_in "Username", with: "tonirib"
+    fill_in "First name", with: "Toni"
+    fill_in "Last name", with: "Rib"
+    fill_in "Email", with: "toni@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "incorrect"
+    click_on "Create My Account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("1 error prohibited this record from being saved")
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
