fee3d28d5bdf0eb9c354b56284e720fd1ecb41ce
ToniRib <tonimarierib@gmail.com>
2016-03-23 21:03:59 -0600

    User can view a current conversation with another user

---
+++ app/assets/javascripts/get-messages.js
@@ -0,0 +1,34 @@
+var getMessages = function(conversation_id){
+  $.ajax({
+    url: "/api/v1/messages",
+    type: "GET",
+    data: {
+      conversation_id: conversation_id
+    },
+    success: function(messages) {
+      messages.forEach(function(message) {
+        appendMessageToPage(message);
+      });
+    }
+  });
+};
+
+var appendMessageToPage = function(message) {
+  $('#messages').append("<div class='row'>" +
+                        "<div class='col-sm-12'>" +
+                        "<div class='row'>" +
+                        "<div class='col-sm-6 left'>" +
+                        "<h4>" + message.username + "</h4>" +
+                        "</div>" +
+                        "<div class='col-sm-6 right'>" +
+                        "<h4>" + message.sent_at + "</h4>" +
+                        "</div>" +
+                        "<div class='row'>" +
+                        "<div class='col-sm-12'>" +
+                        "<p>" + message.body + "</p>" +
+                        "</div>" +
+                        "</div>" +
+                        "</div>" +
+                        "</div>" +
+                        "</div>");
+};
---
+++ spec/features/user_has_a_conversation_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "User has a conversation", type: :feature do
+  scenario "user views a conversation", js: true do
+    conversation = create(:conversation_with_messages)
+    user = conversation.user
+    recipient = conversation.recipient
+    message1, message2 = conversation.messages
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(user)
+
+    visit conversation_path(recipient)
+
+    within "#messages" do
+      expect(page).to have_content(message1.body)
+      expect(page).to have_content(message2.body)
+      expect(page).to have_content(message1.sent_at_date_and_time)
+      expect(page).to have_content(message2.sent_at_date_and_time)
+      expect(page).to have_content(user.username)
+      expect(page).to have_content(recipient.username)
+    end
+  end
+end
